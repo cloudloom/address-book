96fb7ced5385db0acc2ac7f9390c04d2638563c2
vishwa <vishwajit.bhadrashetty@mmpsd.com>
2015-01-02 16:04:25 +0530

    Created Domain model for Email and Phone

--- src/main/java/com/tracebucket/x1/addressbook/domain/Email.java
+++ src/main/java/com/tracebucket/x1/addressbook/domain/Email.java
@@ -1,7 +1,32 @@
 package com.tracebucket.x1.addressbook.domain;
 
+import javax.persistence.Entity;
+
 /**
  * Created by ffl on 02-01-2015.
  */
-public class Email {
+@Entity
+public class Email extends BaseEntity{
+
+    private String userID;
+    private String password;
+
+    public Email() {
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- src/main/java/com/tracebucket/x1/addressbook/domain/Phone.java
+++ src/main/java/com/tracebucket/x1/addressbook/domain/Phone.java
@@ -1,7 +1,50 @@
 package com.tracebucket.x1.addressbook.domain;
 
+import javax.persistence.Entity;
+
 /**
  * Created by ffl on 02-01-2015.
  */
-public class Phone {
+@Entity
+public class Phone extends BaseEntity {
+
+    private String phoneNumber;
+    private String lastName;
+    private String middleName;
+    private String firstName;
+
+    public Phone() {
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 }
